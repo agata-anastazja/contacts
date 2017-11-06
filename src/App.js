28389ad942e32a0101a8d22a3d16a900500aeaed
agata-anastazja <agata.sumowska@gmail.com>
2017-11-06 19:24:55 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        Hiaa!
+      </div>
+    )
+  }
+}
+
+export default App;
